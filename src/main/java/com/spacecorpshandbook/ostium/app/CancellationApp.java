2ab35183264df3d4c6407f433c56b08ca30b7e36
Justin Robison <jrobison151@gmail.com>
2016-11-14 05:48:41 -0500

    Initial commit

---
+++ src/main/java/com/spacecorpshandbook/ostium/app/CancellationApp.java
@@ -0,0 +1,30 @@
+package com.spacecorpshandbook.ostium.app;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+
+@RequestMapping("/")
+public class CancellationApp {
+
+
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+
+        new SpringApplicationBuilder(CancellationApp.class).web(true).run(args);
+    }
+
+}
