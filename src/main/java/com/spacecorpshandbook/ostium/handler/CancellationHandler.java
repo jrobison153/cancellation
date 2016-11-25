41501902f4ccf877057a89cef54257098b4a0d4f
Justin Robison <jrobison151@gmail.com>
2016-11-25 06:34:38 -0500

    AWS Lambda function handler added and build script updated to create different jars for the various application deployment strategies

---
+++ src/main/java/com/spacecorpshandbook/ostium/handler/CancellationHandler.java
@@ -0,0 +1,13 @@
+package com.spacecorpshandbook.ostium.handler;
+
+/**
+ * Amazon Lambda handler adapter for the Cancellation application
+ */
+
+public class CancellationHandler {
+
+    public String cancelAppointment(String appointmentNumber) {
+
+        return "Lambda is awesome!";
+    }
+}
